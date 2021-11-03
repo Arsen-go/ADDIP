f681e89a40a61487688e57f402cce09e763ccae4
Arsen <68658606+Arsen-go@users.noreply.github.com>
2021-11-03 18:21:59 +0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,81 +5,9 @@ const cors = require("cors");
 const { tradeTokenForUser } = require("./auth");
 const typeDefs = require("./typeDefs");
 const { resolvers } = require("./resolvers");
-
 const app = express();
 app.use(cors());
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
-
-// graphql endpoint
-// const server = new ApolloServer({
-//   typeDefs: typeDefs,
-//   resolvers: resolvers,
-//   cacheControl: false,
-//   introspection: true,
-//   playground: true,
-//   context: async ({ req, connection, payload }) => {
-//     let authToken = null;
-//     let currentUser = null;
-//     let currentDriver = null;
-//     let admin = null;
-//     let subscriptionContext = null;
-
-//     try {
-//       authToken = req ? req.headers.authentication : null;
-//       if (connection && connection.context) {
-//         subscriptionContext = connection.context;
-//       }
-
-//       // if (!authToken) {
-//       //   authToken = process.env.TEST_TOKEN;
-//       // }
-
-//       if (authToken) {
-//         const { decodedUser, decodedDriver, decodedAdmin } = await tradeTokenForUser(authToken);
-//         currentUser = decodedUser;
-//         currentDriver = decodedDriver;
-//         admin = decodedAdmin;
-//       }
-//     } catch (e) {
-//       console.log(e);
-//     }
-
-//     return {
-//       authToken,
-//       currentUser,
-//       currentDriver,
-//       admin,
-//       subscriber: subscriptionContext,
-//       ipAddress: req
-//         ? req.connection
-//           ? req.connection.remoteAddress
-//           : ""
-//         : "",
-//     };
-//   },
-//   subscriptions: {
-//     onConnect: async (connectionParams, webSocket, context) => {
-//       if (connectionParams.authentication) {
-//         const { decodedUser, decodedDriver, decodedAdmin } = await tradeTokenForUser(connectionParams.authentication);
-//         currentUser = decodedUser;
-//         currentDriver = decodedDriver;
-//         admin = decodedAdmin;
-//         const context = {
-//           currentUser,
-//           currentDriver,
-//           admin
-//         };
-
-//         return context;
-//       }
-
-//       throw new Error('Missing auth token!');
-//     },
-//     onDisconnect: () => {
-//       console.log("disconnected user");
-//     },
-//   },
-// });
 
 let apolloServer = null;
 async function startServer() {
@@ -123,7 +51,6 @@ startServer();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 const httpServer = http.createServer(app);
-// server.installSubscriptionHandlers(httpServer);
 app.use('/assets', express.static("assets"));
 
 app.listen(process.env.PORT || 4000, () => {
